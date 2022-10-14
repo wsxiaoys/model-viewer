3683358b80a31eda778a97c90d9052f12a34a9f6
Meng Zhang <meng@pixelshift.ai>
2022-10-14 15:43:25 +0800

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,55 @@
+import "./styles.css";
+
+import { useRef, useState, useEffect } from "react";
+import { useControls, button } from "leva";
+
+import ModelEditor from "./ModelEditor";
+
+export default function App() {
+  const ref = useRef();
+  window.ref = ref;
+  const [loaded, setLoaded] = useState(false);
+  useEffect(() => {
+    function onLoad() {
+      setLoaded(true);
+    }
+    if (ref.current) {
+      ref.current.addEventListener("load", onLoad);
+      return () => {
+        ref.current.removeEventListener("load", onLoad);
+      };
+    }
+  }, [ref]);
+
+  const { src } = useControls({
+    src: {
+      value: "airpods_case_exp1.glb",
+      options: [
+        "airpods_case_exp1.glb",
+        "frisbee_exp1.glb",
+        "feedsack_dress.glb",
+        "pillow.glb"
+      ],
+      onChange() {
+        setLoaded(false);
+      },
+      transient: false
+    },
+
+    Export: button(async (get) => {
+      const glTF = await ref.current.exportScene();
+      const file = new File([glTF], "export.glb");
+      const link = document.createElement("a");
+      link.download = file.name;
+      link.href = URL.createObjectURL(file);
+      link.click();
+    })
+  });
+
+  return (
+    <div className="App">
+      {loaded && <ModelEditor viewer={ref.current} />}
+      <model-viewer ref={ref} src={`/${src}`} camera-controls></model-viewer>
+    </div>
+  );
+}
---
+++ src/ModelEditor.js
@@ -0,0 +1,53 @@
+import { useMemo, useEffect } from "react";
+
+import { useControls } from "leva";
+
+export default function ModelEditor({ viewer }) {
+  const { model } = viewer;
+  for (const x of model.materials) {
+    if (x.name.startsWith("Placeholder") || x.name.startsWith("default")) {
+      const name = x.name.replace("Placeholder.", "");
+      const { [name]: image } = useControls({
+        [name]: {
+          image: null
+        }
+      });
+
+      const texture = useMemo(() => {
+        if (image) {
+          return viewer.createTexture(image);
+        }
+      }, [viewer, image]);
+
+      useEffect(() => {
+        if (!texture) return;
+        texture.then((t) => {
+          x.pbrMetallicRoughness["baseColorTexture"].setTexture(t);
+        });
+      }, [texture]);
+    }
+
+    if (x.name.startsWith("object.body.")) {
+      const name = x.name;
+      const v = x.pbrMetallicRoughness.baseColorFactor;
+      const { [name]: color } = useControls({
+        [name]: {
+          r: v[0] * 255,
+          g: v[1] * 255,
+          b: v[2] * 255,
+          a: v[3]
+        }
+      });
+
+      useEffect(() => {
+        x.pbrMetallicRoughness.setBaseColorFactor([
+          color.r / 255,
+          color.g / 255,
+          color.b / 255,
+          color.a
+        ]);
+      }, [color]);
+    }
+  }
+  return <></>;
+}
